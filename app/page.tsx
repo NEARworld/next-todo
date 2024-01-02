31fcfcb1a1590200699aca912082c842ca753123
nearworld <artemismars2@outlook.com>
2024-01-02 16:21:23 +0900

    fetching todos

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
+import { getAllTodos } from '@/api';
 import { AddTask } from './components/AddTask';
 import { TodoList } from './components/TodoList';
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos();
+
   return (
     <main className='max-w-4xl mx-auto mt-4'>
       <div className='text-center my-5 flex flex-col gap-4'>
         <h1 className='text-2xl font-bold'>Next Todo App</h1>
         <AddTask />
-        <TodoList />
+        <TodoList tasks={tasks} />
       </div>
     </main>
   );
