78eb29de68baaf0d54e3160e54009112092b8c3f
nearworld <artemismars2@outlook.com>
2024-01-02 15:40:04 +0900

    the main heading

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from 'next/image';
-
 export default function Home() {
-  return <main>Nextjs</main>;
+  return (
+    <main className='max-w-4xl mx-auto mt-4'>
+      <div className='text-center my-5 flex flex-col gap-4'>
+        <h1 className='text-2xl font-bold'>Next Todo App</h1>
+      </div>
+    </main>
+  );
 }
