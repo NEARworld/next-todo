7e582cb13b39812fca5dca039936737f74cf39e5
nearworld <artemismars2@outlook.com>
2024-01-02 16:43:39 +0900

    passing children prop to Modal component

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -16,7 +16,19 @@ export const AddTask = () => {
         Add new Task <AiOutlinePlus className='ml-2' size={18} />
       </button>
 
-      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+        <form>
+          <h3 className='font-bold text-lg'>Add new task</h3>
+          <div className='modal-action'>
+            <input
+              type='text'
+              placeholder='Type here'
+              className='input input-bordered w-full'
+            />
+            <button className='btn btn-primary'>submit</button>
+          </div>
+        </form>
+      </Modal>
     </div>
   );
 };
