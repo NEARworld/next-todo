f1c9af11f401d9e7d0ebcbfc225b255d862dc4e0
nearworld <artemismars2@outlook.com>
2024-01-02 16:48:06 +0900

    add todo api

--- api.ts
+++ api.ts
@@ -7,3 +7,13 @@ export const getAllTodos = async (): Promise<Task[]> => {
   const todos = await res.json();
   return todos;
 };
+
+export const addTodo = async (todo: Task) => {
+  const res = await fetch(`${baseUrl}/tasks`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(todo),
+  });
+};
