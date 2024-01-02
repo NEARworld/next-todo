0d4f8e26898bc6cc560b7902833d268c22703df3
nearworld <artemismars2@outlook.com>
2024-01-02 16:34:48 +0900

    modalOpen state

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -1,11 +1,22 @@
+'use client';
+
+import { useState } from 'react';
 import { AiOutlinePlus } from 'react-icons/ai';
+import { Modal } from './Modal';
 
 export const AddTask = () => {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
+
   return (
     <div>
-      <button className='btn btn-primary w-full'>
+      <button
+        className='btn btn-primary w-full'
+        onClick={() => setModalOpen(!modalOpen)}
+      >
         Add new Task <AiOutlinePlus className='ml-2' size={18} />
       </button>
+
+      <Modal modalOpen={modalOpen} setModalOpen={setModalOpen} />
     </div>
   );
 };
